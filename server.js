a19b4361ff3b4e346d32d03d8c845276a7d0ee3a
Shama <shamaanjum09@outlook.com>
2018-02-13 13:00:05 -0800

    merge conflicts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express      = require("express");
 const path         = require("path");
-const morgan       = require("morgan");
+const logger       = require("morgan");
 const mongoose     = require("mongoose");
 const bp           = require('body-parser');
 const passport     = require('passport');
