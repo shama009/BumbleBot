ad2776e291197bed150507f57e2f1cc39a075902
chris <chrisb000@gmail.com>
2018-02-13 14:01:06 -0800

    sync

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,82 +1,80 @@
 const Liri = require("../liri/liri");
 const configAuth = require('../config/auth');
+const path = require("path");
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
 
     res.redirect('/');
-    
+
 }
 module.exports = function (app, db, passport) {
-// normal routes ===============================================================
+    // normal routes ===============================================================
 
     // show the home page (will also have our login links)
-    app.get('/', (req, res) =>{
-        res.render('index.ejs');
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/test.html'));
     });
 
     // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, (req, res)=> {
-        res.render('profile.ejs', {
-            user : req.user
-        });
+    app.get('/profile', isLoggedIn, (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/profile.html'));
     });
 
     // LOGOUT ==============================
-    app.get('/logout', (req, res) =>{
+    app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/');
     });
 
     //====================================================
 
-    app.get("/api/twitter/:id/:method/:input", (req, res) => {
+    app.post("/api/twitter", (req, res) => {
         console.log(`endpoint hit`);
         db.User.findOne({
-            id: req.params.id
-        })
+                _id: req.body.id
+            })
             .then(data => {
-                console.log(data.twitter.access_token_key);
-                
+
                 let client = new Liri(
-                    auth.twitterAuth.consumerKey,
-                    auth.twitterAuth.consumerSecret,
+                    configAuth.twitterAuth.consumerKey,
+                    configAuth.twitterAuth.consumerSecret,
                     data.twitter.token,
                     data.twitter.tokenSecret,
-                    data.screenName
+                    data.twitter.username
                 );
 
-                // let client = new Liri(data);
-                let client = new Liri(configAuth.consumerKey, configAuth.consumerSecret, data.twitter.token,data.twitter.tokenSecret, 'cat_retweets');
                 client.init();
+
                 console.log(client);
 
-                switch (req.params.method) {
+                switch (req.body.method) {
 
                     case "get":
-                        setInterval(() => client.get(req.params.input), req.params.frequency);
+                        // setInterval(() => client.get(req.params.input), 5000);
+                        client.get(req.body.input, data => res.json(data));
                         break;
 
                     case "post":
-                        client.post(req.params.input);
+                        client.post(req.body.input, data => res.json(data));
                         break;
 
                     case "fav":
-                        client.fav(req.params.input);
+                        client.fav(req.body.input, data => res.json(data));
                         break;
 
                     default:
-                        console.log("default");
+                        res.send("err no method match");
                         break;
                 }
 
-                res.json(client);
+                // res.json(client);
             })
             .catch(err => res.json(err));
     });
 
-    
+
 
     app.post("/api/twitter/clients/new", (req, res) => {
         // when a user submits a new bot
@@ -92,7 +90,9 @@ module.exports = function (app, db, passport) {
     // passport twitter --------------------------------
 
     // send to twitter to do the authentication
-    app.get('/auth/twitter', passport.authenticate('twitter', { scope: 'email' }));
+    app.get('/auth/twitter', passport.authenticate('twitter', {
+        scope: 'email'
+    }));
 
     // handle the callback after twitter has authenticated the user
     app.get('/auth/twitter/callback',
@@ -101,7 +101,9 @@ module.exports = function (app, db, passport) {
             failureRedirect: '/'
         }));
     // send to twitter to do the authentication
-    app.get('/connect/twitter', passport.authorize('twitter', { scope: 'email' }));
+    app.get('/connect/twitter', passport.authorize('twitter', {
+        scope: 'email'
+    }));
 
     // handle the callback after twitter has authorized the user
     app.get('/connect/twitter/callback',
@@ -110,13 +112,13 @@ module.exports = function (app, db, passport) {
             failureRedirect: '/'
         }));
     // unlink twitter --------------------------------
-    app.get('/unlink/twitter', isLoggedIn,  (req, res) =>{
+    app.get('/unlink/twitter', isLoggedIn, (req, res) => {
         var user = req.user;
         user.twitter.token = undefined;
-        user.save((err)=> {
+        user.save((err) => {
             if (err) throw err;
             res.redirect('/profile');
-           //res.json(user.twitter.token);
+            //res.json(user.twitter.token);
         });
     });
 }
--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const express      = require("express");
-const path         = require("path");
-const morgan       = require("morgan");
-const mongoose     = require("mongoose");
-const bp           = require('body-parser');
-const passport     = require('passport');
-const flash        = require('connect-flash');
-const cookieParser = require('cookie-parser');
-const session      = require('express-session');
-
-=======
 const express  = require("express");
 const path     = require("path");
 const logger   = require("morgan");
@@ -19,38 +7,15 @@ const passport = require('passport');
 const flash    = require('connect-flash');
 const cookieParser = require('cookie-parser');
 const session      = require('express-session');
->>>>>>> master
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-<<<<<<< HEAD
-
-// app.use(express.static("client"));
-
-
-
-// app.set('view engine', 'ejs');
-
-app.use(session({
-    secret: 'ilovescotchscotchyscotchscotch', 
-    resave: true,
-    saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session()); 
-app.use(flash()); 
-
-app.use(morgan('dev'));
-app.use(cookieParser()); 
-=======
 // set up our express application
 app.use(logger('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
->>>>>>> master
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
@@ -66,22 +31,11 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 mongoose.connect("mongodb://localhost/liri");
-<<<<<<< HEAD
-require('./config/passport')(passport);
-const db = require("./models");
-
-require('./controllers/api-routes.js')(app, db, passport);
-
-app.get("/app", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-=======
 require('./config/passport')(passport); // pass passport for configuration
 
 const db = require("./models");
 
 require('./controllers/api-routes.js')(app, db, passport);
->>>>>>> master
 
 // app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./client/build/index.html")));
 
