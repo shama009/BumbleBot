b79a514db8e7b9bd44b71ba98d0469786010334e
lyonx <chrisb000@gmail.com>
2018-03-14 16:56:41 -0700

    deploy

--- config/auth.js
+++ config/auth.js
@@ -6,8 +6,8 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'        : 'VDPgSyJ8vMjSn0PjAZBtQgdPO',
         'consumerSecret'     : '94h1wLHTXPIc3btcZQoBiofmzNbUs2nrUG9c3vUOPsTFcUv3BC',
-        // 'callbackURL'        : 'https://bumble-bot.herokuapp.com/auth/twitter/callback'
-        'callbackURL'        : 'http://localhost:3001/auth/twitter/callback'
+        'callbackURL'        : 'https://bumble-bot.herokuapp.com/auth/twitter/callback'
+        // 'callbackURL'        : 'http://localhost:3001/auth/twitter/callback'
         // 'consumerKey'        : process.env.consumerKey,
         // 'consumerSecret'     : process.env.consumerSecret,
         // 'callbackURL'        : '/auth/twitter/callback'
