e5eade3432eb7971223ea10185b7274f7b043ebe
Shama Anjum <shamaanjum09@outlook.com>
2018-04-13 12:24:39 -0700

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,12 +4,12 @@
 module.exports = {
 
     'twitterAuth' : {
-        //'consumerKey'        : 'VDPgSyJ8vMjSn0PjAZBtQgdPO',
-       // 'consumerSecret'     : '94h1wLHTXPIc3btcZQoBiofmzNbUs2nrUG9c3vUOPsTFcUv3BC',
+        'consumerKey'        : 'VDPgSyJ8vMjSn0PjAZBtQgdPO',
+        'consumerSecret'     : '94h1wLHTXPIc3btcZQoBiofmzNbUs2nrUG9c3vUOPsTFcUv3BC',
         'callbackURL'        : 'https://bumble-bot.herokuapp.com/auth/twitter/callback'
         //'callbackURL'        : 'http://127.0.0.1:3001/auth/twitter/callback'
-         'consumerKey'        : process.env.consumerKey,
-         'consumerSecret'     : process.env.consumerSecret,
+         //'consumerKey'        : process.env.consumerKey,
+         //'consumerSecret'     : process.env.consumerSecret,
          //'callbackURL'        : '/auth/twitter/callback'4
     }
 };
